317bcc8f5be91512b3c57ffd76f383985fa1f105
ElBabakus <124941417+ElBabakus@users.noreply.github.com>
2023-02-16 11:39:06 +0100

    Anpassung Data Frame

--- Aufgabe 1 (Ferry, Tim).R
+++ Aufgabe 1 (Ferry, Tim).R
@@ -18,37 +18,35 @@ Programmierinteresse[which(Studienfach =="Data Science")] <- sample(1:7, length(
 Programmierinteresse[which(Studienfach =="Mathe")] <- sample(1:7, length(which(Studienfach=="Mathe")), replace = TRUE, prob = c(0.5,0.05,0.1,0.1,0.2,0.2,0.3))
 Programmierinteresse[which(Studienfach =="Informatik")] <- sample(1:7, length(which(Studienfach=="Mathe")), replace = TRUE, prob = c(0,0,0,0.05,0.1,0.25,0.6))
 
-
-################################
-# Noch zu tun: Mathe LK ja / nein, Funktion bauen zum Simulieren
-
-#MatheLK
-LK <- sample(c("ja","nein), 100, replace=TRUE, prob = c(0.75,0.25))
+# MatheLK
+MatheLK <- sample(c("ja","nein"), 100, replace=TRUE, prob = c(0.75,0.25))
 
 
-Daten <- function(){
-  Alter <- round(rnorm(100, mean = 25, sd = 2),0)
-  Studienfach <- sample(c("Statistik", "Data Science", "Mathe", "Informatik"), 100, replace = TRUE, prob = c(0.35,0.35,0.20,0.10))
-  Matheinteresse <- rep(0,100)
-  Matheinteresse[which(Studienfach =="Statistik")] <- sample(1:7, length(which(Studienfach=="Mathe")), replace = TRUE, prob = c(0,0,0,0.1,0.15,0.25,0.5))
-  Matheinteresse[which(Studienfach =="Data Science")] <- sample(1:7, length(which(Studienfach=="Mathe")), replace = TRUE, prob = c(0,0.05,0.05,0.1,0.2,0.2,0.4))
-  Matheinteresse[which(Studienfach =="Mathe")] <- sample(1:7, length(which(Studienfach=="Mathe")), replace = TRUE, prob = c(0,0,0,0.1,0.1,0.2,0.6))
-  Matheinteresse[which(Studienfach =="Informatik")] <- sample(1:7, length(which(Studienfach=="Mathe")), replace = TRUE, prob = c(0.05,0.1,0.1,0.1,0.1,0.15,0.4))
-  Programmierinteresse <- rep(0,100)
-  Programmierinteresse[which(Studienfach =="Statistik")] <- sample(1:7, length(which(Studienfach=="Mathe")), replace = TRUE, prob = c(0,0,0.05,0.1,0.15,0.3,0.4))
-  Programmierinteresse[which(Studienfach =="Data Science")] <- sample(1:7, length(which(Studienfach=="Mathe")), replace = TRUE, prob = c(0,0,0,0.1,0.1,0.3,0.5))
-  Programmierinteresse[which(Studienfach =="Mathe")] <- sample(1:7, length(which(Studienfach=="Mathe")), replace = TRUE, prob = c(0.5,0.05,0.1,0.1,0.2,0.2,0.3))
-  Programmierinteresse[which(Studienfach =="Informatik")] <- sample(1:7, length(which(Studienfach=="Mathe")), replace = TRUE, prob = c(0,0,0,0.05,0.1,0.25,0.6))
-  LK <- sample(c("ja", "nein"), 100, replace = TRUE, prob = c(0.75,0.25))
-  
-    
-Daten <- data.frame(matrix(ncol = 6, nrow = 100))
-colnames(Daten) <- c("ID","Alter","Studienfach","Matheinteresse","Programmierinteresse","LK")
-Daten$ID <- 1:100
-Daten$Alter <- Alter
-Daten$Studienfach <- Studienfach
-Daten$Matheinteresse <- Matheinteresse
-Daten$Programmierinteresse <- Programmierinteresse
-Daten$LK <- LK
-return(Daten)
-}
+#Daten <- function(){
+#  Alter <- round(rnorm(100, mean = 25, sd = 2),0)
+#  Studienfach <- sample(c("Statistik", "Data Science", "Mathe", "Informatik"), 100, replace = TRUE, prob = c(0.35,0.35,0.20,0.10))
+#  Matheinteresse <- rep(0,100)
+#  Matheinteresse[which(Studienfach =="Statistik")] <- sample(1:7, length(which(Studienfach=="Mathe")), replace = TRUE, prob = c(0,0,0,0.1,0.15,0.25,0.5))
+#  Matheinteresse[which(Studienfach =="Data Science")] <- sample(1:7, length(which(Studienfach=="Mathe")), replace = TRUE, prob = c(0,0.05,0.05,0.1,0.2,0.2,0.4))
+#  Matheinteresse[which(Studienfach =="Mathe")] <- sample(1:7, length(which(Studienfach=="Mathe")), replace = TRUE, prob = c(0,0,0,0.1,0.1,0.2,0.6))
+#  Matheinteresse[which(Studienfach =="Informatik")] <- sample(1:7, length(which(Studienfach=="Mathe")), replace = TRUE, prob = c(0.05,0.1,0.1,0.1,0.1,0.15,0.4))
+#  Programmierinteresse <- rep(0,100)
+#  Programmierinteresse[which(Studienfach =="Statistik")] <- sample(1:7, length(which(Studienfach=="Mathe")), replace = TRUE, prob = c(0,0,0.05,0.1,0.15,0.3,0.4))
+#  Programmierinteresse[which(Studienfach =="Data Science")] <- sample(1:7, length(which(Studienfach=="Mathe")), replace = TRUE, prob = c(0,0,0,0.1,0.1,0.3,0.5))
+#  Programmierinteresse[which(Studienfach =="Mathe")] <- sample(1:7, length(which(Studienfach=="Mathe")), replace = TRUE, prob = c(0.5,0.05,0.1,0.1,0.2,0.2,0.3))
+#  Programmierinteresse[which(Studienfach =="Informatik")] <- sample(1:7, length(which(Studienfach=="Mathe")), replace = TRUE, prob = c(0,0,0,0.05,0.1,0.25,0.6))
+#  LK <- sample(c("ja", "nein"), 100, replace = TRUE, prob = c(0.75,0.25))
+#  
+#     
+# Daten <- data.frame(matrix(ncol = 6, nrow = 100))
+# colnames(Daten) <- c("ID","Alter","Studienfach","Matheinteresse","Programmierinteresse","LK")
+# Daten$ID <- 1:100
+# Daten$Alter <- Alter
+# Daten$Studienfach <- Studienfach
+# Daten$Matheinteresse <- Matheinteresse
+# Daten$Programmierinteresse <- Programmierinteresse
+# Daten$LK <- LK
+# return(Daten)
+#}
+
+Daten <- data.frame(Alter,MatheLK,Matheinteresse,Programmierinteresse, Studienfach)
